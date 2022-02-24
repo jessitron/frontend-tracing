62c174596401e622c30cc462dbd1c81dbd7b43b7
Jessica Kerr <jessitron@honeycomb.io>
2022-02-24 13:55:04 -0800

    Convert the stored time into a date instead of hrtime
    This is simpler, good idea.
    Also I'm removing the value from sessionStorage so I don't have to worry about additional
    traces getting created if page2 reloads

--- src/page2.js
+++ src/page2.js
@@ -1,64 +1,53 @@
 import "./tracing.js";
 import opentelemetry from "@opentelemetry/api";
 
-/* Copied from https://github.com/open-telemetry/opentelemetry-js/blob/28c9e8829488a7fa131803447b0511195ae1fdf0/packages/opentelemetry-core/src/common/time.ts#L36
- */
-const NANOSECOND_DIGITS = 9;
-const SECOND_TO_NANOSECONDS = Math.pow(10, NANOSECOND_DIGITS);
-function numberToHrtime(epochMillis) {
-  const epochSeconds = epochMillis / 1000;
-  // Decimals only.
-  const seconds = Math.trunc(epochSeconds);
-  // Round sub-nanosecond accuracy to nanosecond.
-  const nanos =
-    Number((epochSeconds - seconds).toFixed(NANOSECOND_DIGITS)) *
-    SECOND_TO_NANOSECONDS;
-  return [seconds, nanos];
-}
-/* end copied from otel */
-
 console.log("Page 2 is here");
 
 // get the start time from session storage!
-const when = Number(window.sessionStorage.getItem("when"));
+const whenString = window.sessionStorage.getItem("when");
+window.sessionStorage.removeItem("when"); // don't repeat this action on reload
 
-// create a span!!
-const span = opentelemetry.trace
-  .getTracer("jess was here")
-  .startSpan("button pushed", {
-    startTime: numberToHrtime(when), // the terrible secret!!!!!!
-    attributes: {
-      // these are all for my own troubleshooting
-      futzing: 160,
-      "app.comment": "hrtime",
-      "app.when": when,
-      "app.hrtime": JSON.stringify(numberToHrtime(when)),
-      "app.my_duration": Date.now() - when,
-      "app.after": Date.now(),
-    },
-  });
-console.log("span is", span); // debugging
-const traceId = span.spanContext().traceId; // debugging
-// end the span!!
-span.end();
+if (whenString) {
+  const when = new Date(Number(whenString));
+  // create a span!!
+  const span = opentelemetry.trace
+    .getTracer("my app")
+    .startSpan("button pushed", {
+      startTime: numberToHrtime(when), // the terrible secret!!!!!!
+      attributes: {
+        // these are all for my own troubleshooting
+        futzing: 160,
+        "app.comment": "converting stored time to date",
+        "app.when": when,
+        "app.hrtime": JSON.stringify(numberToHrtime(when)),
+        "app.my_duration": Date.now() - when,
+        "app.after": Date.now(),
+      },
+    });
+  console.log("span is", span); // debugging
+  const traceId = span.spanContext().traceId; // debugging
+  // end the span!!
+  span.end();
 
-// Now, do something clever that gives the user a link to the trace in honeycomb
-const team = "jessitron"; // change this
-const dataset = "frontend-tracing"; // change this
+  // Now, do something clever that gives the user a link to the trace in honeycomb
+  const team = "jessitron"; // change this
+  const dataset = "frontend-tracing"; // change this
 
-const traceUrl =
-  "https://ui.honeycomb.io/" +
-  team +
-  "/datasets/" +
-  dataset +
-  "/trace?trace_id=" +
-  traceId;
-console.log("specified startTime", traceUrl);
+  const traceUrl =
+    "https://ui.honeycomb.io/" +
+    team +
+    "/datasets/" +
+    dataset +
+    "/trace?trace_id=" +
+    traceId;
+  console.log("Trace URL:", traceUrl);
 
-// create a link in the document
-const putTheTraceLinkHere = document.getElementById("trace-link");
-const traceLink = document.createElement("a");
-const linkText = document.createTextNode("See the newly created trace");
-traceLink.appendChild(linkText);
-traceLink.setAttribute("href", traceUrl);
-putTheTraceLinkHere.replaceChildren(traceLink);
+  // create a link in the document
+  const putTheTraceLinkHere = document.getElementById("trace-link");
+  const traceLink = document.createElement("a");
+  const linkText = document.createTextNode("See the newly created trace");
+  traceLink.appendChild(linkText);
+  traceLink.setAttribute("href", traceUrl);
+  traceLink.setAttribute("target", "_blank");
+  putTheTraceLinkHere.replaceChildren(traceLink);
+}
