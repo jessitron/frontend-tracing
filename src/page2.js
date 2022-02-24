f71532ddebfd1aaae1ea934c7616fb692590c1ba
Jessica Kerr <jessitron@honeycomb.io>
2022-02-23 19:29:41 -0800

    Cleanup

--- src/page2.js
+++ src/page2.js
@@ -1,7 +1,8 @@
 import "./tracing.js";
 import opentelemetry from "@opentelemetry/api";
 
-// COPIED FROM OTEL
+/* Copied from https://github.com/open-telemetry/opentelemetry-js/blob/28c9e8829488a7fa131803447b0511195ae1fdf0/packages/opentelemetry-core/src/common/time.ts#L36
+ */
 const NANOSECOND_DIGITS = 9;
 const SECOND_TO_NANOSECONDS = Math.pow(10, NANOSECOND_DIGITS);
 function numberToHrtime(epochMillis) {
@@ -14,68 +15,48 @@ function numberToHrtime(epochMillis) {
     SECOND_TO_NANOSECONDS;
   return [seconds, nanos];
 }
+/* end copied from otel */
 
 console.log("Page 2 is here");
 
+// get the start time from session storage!
 const when = Number(window.sessionStorage.getItem("when"));
-const my_duration = Date.now() - when;
-const after = Date.now();
-console.log("When was it? ", when);
-console.log("When is it now? ", Date.now());
-console.log("So it took like", my_duration);
 
-const team = "jessitron";
-const dataset = "frontend-tracing";
-
+// create a span!!
 const span = opentelemetry.trace
   .getTracer("jess was here")
   .startSpan("button pushed", {
-    //   startTime: when,
+    startTime: numberToHrtime(when), // the terrible secret!!!!!!
     attributes: {
-      futzing: 73,
+      // these are all for my own troubleshooting
+      futzing: 160,
+      "app.comment": "hrtime",
       "app.when": when,
-      "app.my_duration": my_duration,
-      "app.after": after,
+      "app.hrtime": JSON.stringify(numberToHrtime(when)),
+      "app.my_duration": Date.now() - when,
+      "app.after": Date.now(),
     },
   });
-console.log("span is", span);
+console.log("span is", span); // debugging
+const traceId = span.spanContext().traceId; // debugging
+// end the span!!
+span.end();
 
-const traceId = span.spanContext().traceId;
-url =
-  "http://ui.honeycomb.io/" +
+const team = "jessitron";
+const dataset = "frontend-tracing";
+const traceUrl =
+  "https://ui.honeycomb.io/" +
   team +
   "/datasets/" +
   dataset +
   "/trace?trace_id=" +
   traceId;
-console.log("no startTime", url);
-span.end();
+console.log("specified startTime", traceUrl);
 
-function stuff() {
-  // scope the variables
-  const span = opentelemetry.trace
-    .getTracer("jess was here")
-    .startSpan("button pushed", {
-      startTime: numberToHrtime(when),
-      attributes: {
-        futzing: 160,
-        "app.comment": "hrtime",
-        "app.when": when,
-        "app.my_duration": my_duration,
-        "app.after": after,
-      },
-    });
-  console.log("span is", span);
-
-  const traceId = span.spanContext().traceId;
-  url =
-    "http://ui.honeycomb.io/" +
-    team +
-    "/datasets/" +
-    dataset +
-    "/trace?trace_id=" +
-    traceId;
-  console.log("specified startTime", url);
-  span.end();
-}
-stuff();
+// create a link in the document
+const putTheTraceLinkHere = document.getElementById("trace-link");
+const traceLink = document.createElement("a");
+const linkText = document.createTextNode("See the newly created trace");
+traceLink.appendChild(linkText);
+traceLink.setAttribute("href", traceUrl);
+putTheTraceLinkHere.replaceChildren(traceLink);
