3a7f29db805a49da334b03fc8f35c847c675b684
Jessica Kerr <jessitron@honeycomb.io>
2022-02-23 18:36:46 -0800

    I was getting spans there for a bit

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ console.log("app was here");
 const button = document.getElementById("how-long-does-this-take");
 button.onclick = () => {
   console.log("thanks for clicking");
-  window.sessionStorage.setItem('when', Date.now())
-  window.location.href = "/page2.html";
+  window.sessionStorage.setItem("when", Date.now());
+  // now be slow
+  setTimeout(() => {
+    window.location.href = "/page2.html";
+  }, 500);
 };
--- src/page2.js
+++ src/page2.js
@@ -1,6 +1,29 @@
 import "./tracing.js";
+import opentelemetry from "@opentelemetry/api";
 
 console.log("Page 2 is here");
 
-const when = window.sessionStorage.getItem("when");
+const when = Number(window.sessionStorage.getItem("when"));
+const duration = Date.now() - when;
 console.log("When was it? ", when);
+
+const span = opentelemetry.trace
+  .getTracer("jess was here")
+  .startSpan("button pushed", {
+    startTime: when,
+    attributes: { duration_ms: duration, futzing: 73 },
+  });
+console.log("span is", span);
+
+const team = "jessitron";
+const dataset = "frontend-tracing";
+const traceId = span.spanContext().traceId;
+url =
+  "http://ui.honeycomb.io/" +
+  team +
+  "/datasets/" +
+  dataset +
+  "/trace?trace_id=" +
+  traceId;
+console.log(url);
+span.end();
