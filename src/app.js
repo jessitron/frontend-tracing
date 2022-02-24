1e96ed723bbb20ca0286352e800c7ee54b199a07
Jessica Kerr <jessitron@honeycomb.io>
2022-02-23 18:19:05 -0800

    Store timestamp in session storage

--- src/app.js
+++ src/app.js
@@ -5,5 +5,6 @@ console.log("app was here");
 const button = document.getElementById("how-long-does-this-take");
 button.onclick = () => {
   console.log("thanks for clicking");
+  window.sessionStorage.setItem('when', Date.now())
   window.location.href = "/page2.html";
 };
--- src/page2.js
+++ src/page2.js
@@ -1,3 +1,6 @@
 import "./tracing.js";
 
 console.log("Page 2 is here");
+
+const when = window.sessionStorage.getItem("when");
+console.log("When was it? ", when);
